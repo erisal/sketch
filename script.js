03fc81d55c9bc1fcca51a7bbc971e244360e00d7
erisal <erisal@gmail.com>
2021-01-22 13:08:01 -0800

    first pass complete

---
+++ script.js
@@ -0,0 +1,49 @@
+var pixelCount = 64;
+var minPxl = 10;
+var maxPxl = 100;
+var gridCount = pixelCount * pixelCount;
+let height = window.innerHeight;
+let width = window.innerWidth;
+let containerSize;
+if (height >= width) containerSize = width - 30;
+else containerSize = height - 100;
+var pixelSize = Math.round(containerSize / pixelCount) - 2;
+containerSize = pixelCount * (pixelSize + 2);
+console.log(pixelSize);
+console.log(containerSize + "px");
+console.log(screen.availHeight);
+
+var container = document.querySelector('#gridContainer');
+container.setAttribute('style', `width: ${containerSize}px`);
+container.addEventListener('mouseover', hovered)
+makePixels();
+
+function makePixels () { 
+    for (let i = 0; i < gridCount; i++) {
+        let pixel = document.createElement('div');
+        pixel.setAttribute('style', `width: ${pixelSize}px; height: ${pixelSize}px`)
+        pixel.id = i;
+        pixel.classList.add('pixel');
+        container.appendChild(pixel);
+    }
+}
+
+function hovered(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+function reset () {
+    pixelCount = Math.round(prompt('Enter resolution (10 - 100)'));
+    if (pixelCount < minPxl || pixelCount > maxPxl || isNaN(pixelCount)) {
+        reset();
+    }
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    gridCount = pixelCount * pixelCount;
+
+    pixelSize = Math.round(containerSize / pixelCount) - 2;
+    containerSize = pixelCount * (pixelSize + 2);
+    container.setAttribute('style', `width: ${containerSize}px`);
+    makePixels()
+}
